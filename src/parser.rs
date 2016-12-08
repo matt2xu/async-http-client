dd16eaeb7b6d970024f1353329b9e9878c042d4d
Matthieu Wipliez <matthieu.wipliez@harmonicinc.com>
2016-12-08 15:16:58 +0100

    fix grammar for major.minor digits

--- src/parser.rs
+++ src/parser.rs
@@ -1,49 +1,51 @@
 
-use nom::{IResult, ErrorKind, crlf, digit};
-use nom::IResult::{Incomplete, Done, Error};
+use nom::{IResult, ErrorKind, crlf, is_digit};
+use nom::IResult::{Done, Error};
 
 use std::str;
 
-fn ret_digit(input: &[u8]) -> IResult<&[u8], u32> {
-    let result = digit(input);
-    result.map(|slice| (slice[0] as char).to_digit(10).unwrap_or(0))
+fn parse_code(input: &[u8]) -> IResult<&[u8], u32> {
+    if input.iter().all(|&c| is_digit(c)) {
+        let sum = input.iter().fold(0, |sum, &c| sum * 10 + (c as char).to_digit(10).unwrap_or(0));
+        Done(&input[3..], sum)
+    } else {
+        Error(ErrorKind::Digit)
+    }
 }
 
-fn code(input: &[u8]) -> IResult<&[u8], u32> {
-    match is_a!(input, &b"0123456789"[..]) {
-        Error(e)    => Error(e),
-        Incomplete(e) => Incomplete(e),
-        Done(i, o) => {
-            if o.len() != 3 {
-                return Error(ErrorKind::Digit);
-            }
-
-            Done(i, str::from_utf8(o).unwrap_or("0").parse().unwrap_or(0))
-        }
-    }
+pub enum StatusKind {
+    Informational,
+    Successful,
+    Redirection,
+    ClientError,
+    ServerError
 }
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct StatusLine {
+    pub major: u32,
+    pub minor: u32,
     pub code: u32
 }
 
 named!(pub status_line<StatusLine>,
     do_parse!(
         tag!("HTTP/") >>
-        d1 : ret_digit >>
+        major: take_while1!(is_digit) >>
         char!('.') >>
-        d2 : ret_digit >>
+        minor: take_while1!(is_digit) >>
         char!(' ') >>
-        code: flat_map!(take!(3), code) >>
+        code: flat_map!(take!(3), parse_code) >>
         char!(' ') >>
         reason: take_until!("\r\n") >>
         call!(crlf) >>
         ({
-            assert!(d1 == 1);
-            assert!(d2 == 1);
+            let major = unsafe { str::from_utf8_unchecked(major) }.parse().unwrap_or(0);
+            let minor = unsafe { str::from_utf8_unchecked(minor) }.parse().unwrap_or(0);
             println!("reason: {}", str::from_utf8(reason).unwrap_or("invalid reason"));
             StatusLine {
+                major: major,
+                minor: minor,
                 code: code
             }
         })
@@ -60,6 +62,6 @@ mod tests {
     #[test]
     fn test_status_line() {
         assert_eq!(status_line(&b"HTTP/1.1 404 Not Found\r\n"[..]),
-            Done(&b""[..], StatusLine {code: 404}));
+            Done(&b""[..], StatusLine {major: 1, minor:1, code: 404}));
     }
 }
