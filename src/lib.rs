551f9617570efb5202984f19fa03c2a41915a348
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2016-12-13 22:52:31 +0100

    add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,31 @@
 //! Asynchronous HTTP client.
+//!
+//! ## Installation
+//!
+//! Add this to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! async-http-client = "0.1"
+//! ```
+//! ## Example
+//!
+//! ```
+//! extern crate async_http_client;
+//!
+//! use async_http_client::prelude::*;
+//! use async_http_client::{HttpRequest, HttpCodec};
+//!
+//! let req = HttpRequest::get("http://www.google.com").unwrap();
+//! let mut core = Core::new().unwrap();
+//! let addr = req.addr().unwrap();
+//! let handle = core.handle();
+//! let (res, framed) = core.run(TcpStream::connect(&addr, &handle).and_then(|connection| {
+//!     let framed = connection.framed(HttpCodec::new());
+//!     req.send(framed)
+//! })).unwrap();
+//! println!("got response {}", res.unwrap());
+//! ```
 
 extern crate futures;
 extern crate tokio_core;
